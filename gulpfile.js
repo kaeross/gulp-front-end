15d266a17b3d785de1c324c82abb9a7d439bd7a0
Kate Ross <kaeross@icloud.com>
2017-11-20 17:05:04 +0000

    add sass to css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,56 @@
 "use strict";
 
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+let gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    maps = '../../maps',
+    cleanCSS = require('gulp-clean-css');
 
-//concatenates, minifies and copies to dist/scripts folder
-gulp.task("scripts", function() {
+//concatenates, minifies and copies js to dist/scripts folder
+gulp.task("scripts", function () {
     console.log('run scripts task');
     return gulp.src([
-        'js/*.js',
-        'js/circle/*.js'])
-        //concatenate
+            'js/*.js',
+            'js/circle/*.js'
+        ])
+        .pipe(sourcemaps.init())
         .pipe(concat('all.min.js'))
         .pipe(uglify('all.min.js'))
+        .pipe(sourcemaps.write(maps))
         .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task("styles", function() {
+//concatenates, minifies and copies scss to dist/styles folder
+gulp.task("styles", function () {
     console.log('run styles task');
+    return gulp.src([
+            'sass/*.scss',
+            'sass/circle/*.scss',
+            'sass/circle/components/*.scss',
+            'sass/circle/core/*.scss',
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(rename('all.min.css'))
+        .pipe(sourcemaps.write(maps))
+
+        //.pipe(cleanCSS)
+        .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task("images", function() {
+gulp.task("images", function () {
     console.log('run images task');
 });
 
-gulp.task("clean", function() {
+gulp.task("clean", function () {
     console.log('run clean task');
 });
 
-gulp.task("build", function() {
-    
-});
+gulp.task("build", ['clean', 'scripts', 'styles', 'images']);
 
-gulp.task('default', ['build'], function() {
-
+gulp.task('default', ['build'], function () {
+    console.log('run all tasks');
 });
