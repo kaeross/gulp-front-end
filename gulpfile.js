d14bb61e2049ecce8ac1dbb7f39400540b1a9e9a
Kate Ross <kaeross@icloud.com>
2017-11-21 11:07:06 +0000

    Create src folder and move files

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 "use strict";
 
-let gulp = require('gulp'),
+const gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
     sourcemaps = require('gulp-sourcemaps'),
-    maps = './';
+    maps = './',
+    rm = require('gulp-rm'),
+    imagemin = require('gulp-imagemin'),
+    webserver = require('gulp-webserver'),
+    connect = require('gulp-connect');
 
 //concatenates, minifies and copies js to dist/scripts folder
 gulp.task("scripts", function () {
-    console.log('run scripts task');
     return gulp.src([
             'js/*.js',
             'js/circle/*.js'
@@ -24,7 +27,6 @@ gulp.task("scripts", function () {
 
 //concatenates, minifies and copies scss to dist/styles folder
 gulp.task("styles", function () {
-    console.log('run styles task');
     return gulp.src([
             'sass/*.scss',
             'sass/circle/*.scss',
@@ -32,26 +34,53 @@ gulp.task("styles", function () {
             'sass/circle/core/*.scss',
         ])
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
         .pipe(rename('all.min.css'))
         .pipe(sourcemaps.write(maps))
         .pipe(gulp.dest('dist/styles'));
 });
 
+//optimises images and copies to dist/content folder
 gulp.task("images", function () {
-    console.log('run images task');
+    return gulp.src('images/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({
+                interlaced: true
+            }),
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            imagemin.optipng({
+                optimizationLevel: 1
+            }),
+        ]))
+        .pipe(gulp.dest('dist/content'));
 });
 
 gulp.task("clean", function () {
-    console.log('run clean task');
+    return gulp.src('dist/**/*', {
+            read: false
+        })
+        .pipe(rm({
+            async: false
+        }));
 });
 
 gulp.task("watch", function () {
-    gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['styles']);
 });
 
-gulp.task("build", ['clean', 'scripts', 'styles', 'images']);
-
-gulp.task('default', ['build', 'watch'], function () {
-    
+gulp.task("serve", function() {
+    connect.server();
 });
+
+gulp.task("build", ['clean'], function () {
+    gulp.start(['scripts', 'styles', 'images']);
+
+});
+
+gulp.task('default', ['build'], function() {
+    gulp.start(['watch', 'serve']);
+}); 
