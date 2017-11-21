8fb6868b4d5c8bc2e72f927625df4f7756173225
Kate Ross <kaeross@icloud.com>
2017-11-21 15:41:01 +0000

    Update sequences and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,78 @@
 "use strict";
 
 const gulp = require('gulp'),
-    concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     sourcemaps = require('gulp-sourcemaps'),
     maps = './',
-    rm = require('gulp-rm'),
+    del = require('del'),
     imagemin = require('gulp-imagemin'),
-    webserver = require('gulp-webserver'),
+    sequence = require('run-sequence'),
     connect = require('gulp-connect');
+
+var options = {
+    src: 'src',
+    dist: 'dist'
+};
+var paths = {
+    styles: {
+        src: [
+            options.src + '/sass/*.scss',
+            options.src + '/sass/circle/*.scss',
+            options.src + '/sass/circle/components/*.scss',
+            options.src + '/sass/circle/core/*.scss',
+        ],
+        dest: options.dist + '/styles'
+    },
+    scripts: {
+        src: [
+            options.src + '/js/*.js',
+            options.src + '/js/circle/*.js'
+        ],
+        dest: options.dist + '/scripts'
+    },
+    icons: {
+        src: options.src + '/icons/*',
+        dest: options.dist + '/content/icons'
+    },
+    images: {
+        src: options.src + '/images/**/*',
+        dest: options.dist + '/content/images'
+    }
+
+};
 
 //concatenates, minifies and copies js to dist/scripts folder
 gulp.task("scripts", function () {
-    return gulp.src([
-            'js/*.js',
-            'js/circle/*.js'
-        ])
+    return gulp.src(paths.scripts.src)
         .pipe(sourcemaps.init())
         .pipe(concat('all.min.js'))
         .pipe(uglify('all.min.js'))
         .pipe(sourcemaps.write(maps))
-        .pipe(gulp.dest('dist/scripts'));
+        .pipe(gulp.dest(paths.scripts.dest));
 });
 
 //concatenates, minifies and copies scss to dist/styles folder
 gulp.task("styles", function () {
-    return gulp.src([
-            'sass/*.scss',
-            'sass/circle/*.scss',
-            'sass/circle/components/*.scss',
-            'sass/circle/core/*.scss',
-        ])
+    return gulp.src(paths.styles.src)
         .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
         }))
         .pipe(rename('all.min.css'))
         .pipe(sourcemaps.write(maps))
-        .pipe(gulp.dest('dist/styles'));
+        .pipe(gulp.dest(paths.styles.dest));
 });
 
+gulp.task('moveIcons', function () {
+    gulp.src(paths.icons.src)
+        .pipe(gulp.dest(paths.icons.dest));
+});
 //optimises images and copies to dist/content folder
-gulp.task("images", function () {
-    return gulp.src('images/*')
+gulp.task("images", ['moveIcons'], function () {
+    return gulp.src(paths.images.src)
         .pipe(imagemin([
             imagemin.gifsicle({
                 interlaced: true
@@ -56,31 +84,32 @@ gulp.task("images", function () {
                 optimizationLevel: 1
             }),
         ]))
-        .pipe(gulp.dest('dist/content'));
+        .pipe(gulp.dest(paths.images.dest));
 });
 
 gulp.task("clean", function () {
-    return gulp.src('dist/**/*', {
-            read: false
-        })
-        .pipe(rm({
-            async: false
-        }));
+    del('dist/*');
 });
 
 gulp.task("watch", function () {
-    gulp.watch(['sass/**/*.sass', 'sass/**/*.scss'], ['styles']);
+    gulp.watch([options.src + '/sass/**/*.sass', options.src + '/sass/**/*.scss'], ['styles'])
+    .pipe(connect.reload());
 });
 
-gulp.task("serve", function() {
-    connect.server();
+gulp.task("build", function () {
+    sequence('clean', ['scripts', 'styles', 'images'], function() {
+        return gulp.src(options.src + '/index.html')
+        .pipe(gulp.dest(options.dist + '/'));
+    });
+    
 });
 
-gulp.task("build", ['clean'], function () {
-    gulp.start(['scripts', 'styles', 'images']);
-
+gulp.task('default', function () {
+    sequence('build', 'watch', function () {
+        connect.server({
+            port: 8080,
+            root: 'dist',
+            livereload: true
+        });
+    });
 });
-
-gulp.task('default', ['build'], function() {
-    gulp.start(['watch', 'serve']);
-}); 
